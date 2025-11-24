3c13ebd6f9b0e52cb0595f33ae12485de5919322
Mahi871 <144423296+Mahi871@users.noreply.github.com>
2025-11-24 16:44:36 +0000

    conversion operation done

---
+++ variables/conversionOperation.js
@@ -0,0 +1,19 @@
+
+let numbers = 33
+
+ newNumbers = toString(numbers)
+console.log(typeof newNumbers)
+
+let myName = "ganesh kalle"
+
+ newMYName= Number(myName)
+ console.log( typeof newMYName)
+
+ let move = true
+  Dotmove= Number(move)
+  console.log( typeof Dotmove)
+
+
+  // datatyopes conversion 
+  //to convert string into number use the Number Key for converting 
+    
---
+++ variables/test1.js
@@ -0,0 +1,15 @@
+const name = "Mahessh"
+let rollNO = 19
+var id = "rajeraje433@gmail.com"
+password = 905845993
+
+
+
+
+// name ="raj"
+rollNO = 26
+id ="code@2434.com"
+password = 604043
+
+
+console.table([name , rollNO ,id ,password])
