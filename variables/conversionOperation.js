30211cac2dc8b236814a7e058f93439b3f86ab8f
Mahi871 <144423296+Mahi871@users.noreply.github.com>
2025-11-25 17:13:35 +0000

    conversion operaation is done

--- variables/conversionOperation.js
+++ variables/conversionOperation.js
@@ -2,20 +2,31 @@
 let numbers = 33
 
  newNumbers = toString(numbers)
-console.log(typeof newNumbers)
+// console.log(typeof newNumbers)
 
 let myName = "ganesh kalle"
 
  newMYName= Number(myName)
- console.log( typeof newMYName)
+//  console.log( typeof newMYName)
 
  let move = true
   Dotmove= Number(move)
-  console.log( typeof Dotmove)
+//   console.log( typeof Dotmove)
 
-  console.log(myName)
+//   console.log(myName)
 
 
   // datatyopes conversion 
   //to convert string into number use the Number Key for converting 
-    
+
+let x= 3;
+let y= x++;
+
+console.log(`x: ${x}, y: ${y}`); // x: 4, y: 3
+// The postfix increment operator (x++) returns the value before incrementing it.
+
+let a= 5;
+let b= ++a;
+
+console.log(`a: ${a}, b: ${b}`); // a: 6, b: 6
+// The prefix increment operator (++a) increments the value first and then returns it.
